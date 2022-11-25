0c1e5c24e27a4c535dd79516d98daca3594f95b0
jesseClegg <89627047+jesseClegg@users.noreply.github.com>
2022-11-24 19:32:16 -0500

    added starting 3 profiles

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,9 +14,9 @@ mongoose.connect("mongodb://mongo:27017/mongocontainer",
     e=> console.error(e)
 )
 
-createNewProfile("Sponge Robert", "https://s3forninad.s3.amazonaws.com/mockingspongebobbb.jpg", "The best time to wear a striped sweater is all the time." );
-createNewProfile("Eugene Krabs", "https://s3forninad.s3.amazonaws.com/mrKrabsIsaRobot.jpg", "Hey, that's pretty catchy. Bee-bee-boo-bop, bee-bee-boo-beep. Yeah, that's not bad. I love this young people's music.");
-createNewProfile("Karen", "https://s3forninad.s3.amazonaws.com/karen.jpg", "Plankton, get the secret formula");
+ createNewProfile("Sponge Robert", "https://s3forninad.s3.amazonaws.com/mockingspongebobbb.jpg", "The best time to wear a striped sweater is all the time." );
+ createNewProfile("Eugene Krabs", "https://s3forninad.s3.amazonaws.com/mrKrabsIsaRobot.jpg", "Hey, that's pretty catchy. Bee-bee-boo-bop, bee-bee-boo-beep. Yeah, that's not bad. I love this young people's music.");
+ createNewProfile("Karen", "https://s3forninad.s3.amazonaws.com/karen.jpg", "Plankton, get the secret formula");
 // mongoose.connect("mongodb+srv://ass4:mongo4998@cluster0.dqr9rtj.mongodb.net/?retryWrites=true&w=majority",
 //     ()=> {
 //         //console.log("connection in a pineapple under the sea")
